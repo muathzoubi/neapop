aff9774b2f562b266896d66d6bb338c51026584c
muathzoubi <moathaza@gmail.com>
2024-03-21 01:48:13 +0300

    fasr

--- app/viewes/Home.tsx
+++ app/viewes/Home.tsx
@@ -1,4 +1,5 @@
-import Home2 from './viewes/Home';
+import Home2 from "../page";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-16">
